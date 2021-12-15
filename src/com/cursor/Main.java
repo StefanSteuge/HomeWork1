f6dee2750b329198fab987b5530372b9c9a98b69
Stepan Dulka <fotosteuge@gmail.com>
2021-12-15 17:18:56 +0200

    Init commit for HW1

---
+++ src/com/cursor/Main.java
@@ -0,0 +1,21 @@
+package com.cursor;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a, b, e, x, X, y, w;
+        a = 2;
+        b = 7;
+        e = 15;
+        x = 58;
+        X = 567;
+        y = 75;
+        w = 106;
+
+        System.out.println(x + y - a / 2 * (x + w));
+        System.out.println(a * (x + x) / y - a * 3);
+        System.out.println(a + b / x - y);
+        System.out.println(X - e / 2 % e + 1);
+
+    }
+}
