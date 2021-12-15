d8f15bf7889facffc4054245ae6cedd9e5e956b2
Stepan Dulka <fotosteuge@gmail.com>
2021-12-15 19:13:57 +0200

    add expression

--- src/com/cursor/Main.java
+++ src/com/cursor/Main.java
@@ -11,11 +11,16 @@ public class Main {
         X = 567;
         y = 75;
         w = 106;
+        String S = "x + y - a / 2 * (x + w)";
+        String S1 = "a * (x + x) / y - a * 3";
+        String S2 = "a + b / x - y";
+        String S3 = "X - e / 2 % e + 1";
 
-        System.out.println(x + y - a / 2 * (x + w));
-        System.out.println(a * (x + x) / y - a * 3);
-        System.out.println(a + b / x - y);
-        System.out.println(X - e / 2 % e + 1);
+
+        System.out.println(S + ' ' + "result is" + ' ' + (x + y - a / 2 * (x + w)));
+        System.out.println(S1 + ' ' + "result is" + ' ' + (a * (x + x) / y - a * 3));
+        System.out.println(S2 + ' ' + "result is" + ' ' + (a + b / x - y));
+        System.out.println(S3 + ' ' + "result is" + ' ' + (X - e / 2 % e + 1));
 
     }
 }
